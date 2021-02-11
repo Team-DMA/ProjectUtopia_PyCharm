c4e4a822565d44b40f7581d420e2293c027173d4
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-02-11 11:29:41 +0100

    Test GPS 1

--- gps_test.py
+++ gps_test.py
@@ -1,17 +1,13 @@
 import serial
-import time
-import string
 import pynmea2
 
-while True:
-    port = "/dev/ttyAMA0"
-    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
+def parseGPS(str):
+    if str.find('GGA') > 0:
+        msg = pynmea2.parse(str)
+        print ("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
 
-    if newdata[0:6] == "$GPRMC":
-        newmsg = pynmea2.parse(newdata)
-        lat = newmsg.latitude
-        lng = newmsg.longitude
-        gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
-        print(gps)
+serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+
+while True:
+    str = serialPort.readline()
+    parseGPS(str)
