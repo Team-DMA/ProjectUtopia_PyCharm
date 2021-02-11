97a2911ca4ab638817acb38d99428f231c9e02ce
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-02-11 11:48:55 +0100

    Test GPS 23

--- gps_test.py
+++ gps_test.py
@@ -1,16 +1,39 @@
 import serial
-import pynmea2
+from time import sleep
+import sys
 
+ser = serial.Serial(“/dev/ttyS0”)
+gpgga_info = “$GPGGA,”
+GPGGA_buffer = 0
+NMEA_buff = 0
 
-def parseGPS(str2):
-    if str2.find('GGA') > 0:
-        msg = pynmea2.parse(str2)
-        print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (
-            msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units))
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value – int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = “%.4f” %(position)
+    return position
 
+try:
+    while True:
+        received_data = (str)(ser.readline()) #read NMEA string received
+        GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string
+        if (GPGGA_data_available>0):
+            GPGGA_buffer = received_data.split(“$GPGGA,”,1)[1]  #store data coming after “$GPGGA,” string
+            NMEA_buff = (GPGGA_buffer.split(‘,’))
+            nmea_time = []
+            nmea_latitude = []
+            nmea_longitude = []
+            nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
+            nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
+            nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
+            print(“NMEA Time: “, nmea_time,’\n’)
+            lat = (float)(nmea_latitude)
+            lat = convert_to_degrees(lat)
+            longi = (float)(nmea_longitude)
+            longi = convert_to_degrees(longi)
+            print (“NMEA Latitude:”, lat,”NMEA Longitude:”, longi,’\n’)
 
-serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
-
-while True:
-    str1 = serialPort.readline()
-    parseGPS(str1)
+except KeyboardInterrupt:
+    sys.exit(0)
