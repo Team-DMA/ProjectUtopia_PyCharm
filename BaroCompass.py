a3c5977ad561077d42f9ebf39d203ac3c8a89bd6
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-02-11 12:28:26 +0100

    Test GPS 23

--- BaroCompass.py
+++ BaroCompass.py
@@ -12,7 +12,6 @@ from i2clibraries import i2c_hmc5883l
 class BAROMETER(object):
 
     def __init__(self):
-
         # Get I2C bus
         self.bus = smbus.SMBus(1)
 
@@ -21,8 +20,7 @@ class BAROMETER(object):
         self.pressure = 0
         self.altitude = 0
 
-    def gettemp(self):    
-
+    def gettemp(self):
         # Send OSR and channel setting command, 0x44(68)
         self.bus.write_byte(0x76, 0x44 | 0x00)
         time.sleep(0.1)
@@ -31,9 +29,8 @@ class BAROMETER(object):
         # Convert the data to 20-bits
         self.cTemp = (((data[0] & 0x0F) * 65536) + (data[1] * 256) + data[2]) / 100.00
         self.pressure = (((data[3] & 0x0F) * 65536) + (data[4] * 256) + data[5]) / 100.00
-    
-    def getaltitude(self):
 
+    def getaltitude(self):
         # HP206C address, 0x76(118)
         # Send OSR and channel setting command, 0x44(68)
         self.bus.write_byte(0x76, 0x44 | 0x01)
@@ -47,34 +44,30 @@ class BAROMETER(object):
 
         # Convert the data to 20-bits
         self.altitude = (((data[0] & 0x0F) * 65536) + (data[1] * 256) + data[2]) / 100.00
-  
-    def Temperature(self):
 
+    def Temperature(self):
         self.gettemp()
         return self.cTemp
 
     def Pressure(self):
-
         self.gettemp()
         return self.pressure
 
     def Altitude(self):
-
         self.getaltitude()
         return self.altitude
 
 
 class COMPASS(object):
     def __init__(self):
-     
         self.hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)  # if not the first I2C Device, the 1 has to be changed
 
         self.hmc5883l.setContinuousMode()
         self.hmc5883l.setDeclination(2, 15)
 
     def Compass(self):
-
-        return self.hmc5883l.getHeadingString()
+        tmp123 = self.hmc5883l.getAxes()
+        return tmp123
 
 
 temp1 = BAROMETER()
