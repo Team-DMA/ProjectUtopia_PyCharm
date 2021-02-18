694b437a95fdf47243f837927661cb4bd3aeb2e2
bergungsdackel <mateusz-koziel@t-online.de>
2021-02-18 13:25:36 +0100

    bugfix

--- Gyro.py
+++ Gyro.py
@@ -59,7 +59,7 @@ class GYRO(object):
         if value >= 0x8000:
             return -((65535 - value) + 1)
         else:
-            return val
+            return value
 
     def dist(self, a, b):
 
