72969f329abfa688ea7ad3c9cb7415e8b3095d31
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-02-18 12:12:33 +0100

    Test GPS 1

--- gps_richtig.py
+++ gps_richtig.py
@@ -28,11 +28,20 @@ class GPS(object):
         print(str(newData))
         # if "$GPGLL" in str(newData):
         #    print(str(newData))
-        tmp1, tmp2 = newData.split("GPGLL,")
-        tmp2 = str(tmp2)
-        Norden, tmp4, Osten, tmp5 = tmp2.split(",")
-        print("N: %d" % Norden + ", E: %d" % Osten)
+        if newData.find("GPGLL"):
+            tmp1, tmp2 = newData.split("GPGLL,")
+            tmp2 = str(tmp2)
+            Norden, tmp4, Osten, tmp5 = tmp2.split(",")
+            print("N: %d" % Norden + ", E: %d" % Osten)
+        #if newData.find('GPGLL'):
+        #    print("juhu")
 
+        #if newData[3:8] == "$GPGLL":
+        #    print("juhu2")
+        #if newData[0:8] == "b'$GPGLL":
+        #    print("juhu3")
+        #if newData[0:2] == "b'":
+        #    print("juhu4")
         #  newMessage = pynmea2.parse(newData)
         # self.lat = newMessage.latitude
         # self.lng = newMessage.longitude
