9a9c29123550b779f7c1a44408950eb85c87642e
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-03-04 10:25:46 +0100

    pimmel riecht.

--- AnalogDigitalConverter.py
+++ AnalogDigitalConverter.py
@@ -2,7 +2,9 @@ from ADS1x15 import ADS1015
 import threading
 import time
 
+
 class ANALOG_DIGITAL_CONVERTER(threading.Thread):
+
     def __init__(self):
         threading.Thread.__init__(self)
         self.daemon = True
@@ -10,14 +12,16 @@ class ANALOG_DIGITAL_CONVERTER(threading.Thread):
         self.channel = 0
         self.gain = 1
         self.data = 0
-        self.start()
         self.voltage = 0
-
+        self.start()
 
     def run(self):
         while True:
             self.data = self.adc.read_adc(self.channel, self.gain)
-            #1Bit=3mV
-            self.voltage = 33000/((self.data/3)/1000*20000)
-
+            # 1Bit=3mV
+            self.voltage = ((self.data / 3) / 1000 * (33000 + 20000)) / 20000
+            print("Battery Voltage: " + str(self.voltage))
             time.sleep(0.1)
+
+
+tmp = ANALOG_DIGITAL_CONVERTER()
