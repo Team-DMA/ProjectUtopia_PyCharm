3d43717511a2549d80a8635f7a8fd824fedd6301
dominikguba <56926355+dominikguba@users.noreply.github.com>
2021-03-04 10:57:59 +0100

    spannung messen

--- AnalogDigitalConverter.py
+++ AnalogDigitalConverter.py
@@ -3,6 +3,20 @@ import threading
 import time
 
 
+def scale(old_value, old_min, old_max, new_min, new_max):
+    new_value = ((old_value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
+    return new_value
+
+
+def clamp(value, minOut, maxOut):
+    if value > maxOut:
+        return maxOut
+    elif value < minOut:
+        return minOut
+
+    return value
+
+
 class ANALOG_DIGITAL_CONVERTER(threading.Thread):
 
     def __init__(self):
@@ -13,18 +27,19 @@ class ANALOG_DIGITAL_CONVERTER(threading.Thread):
         self.gain = 1
         self.data = 0
         self.voltage = 0
+        self.percentage = 0
         self.start()
 
     def run(self):
         while True:
             self.data = self.adc.read_adc(self.channel, self.gain)
             # 1Bit=3mV
-            print(str(self.data/490))
-            self.voltage = ((self.data/490) / 19700) * 32900 + ((self.data/490))
-            print("Battery VoltageL: " + str(self.voltage))
-            self.voltage = self.data/155
-            print("Battery VoltageD: " + str(self.voltage))
+            self.voltage = self.data / 155
+            self.percentage = scale(self.voltage, 10, 12.5, 0, 100)
+            self.percentage = clamp(self.percentage, 0, 100)
             time.sleep(0.1)
+
+            print("Voltage: " + str(self.voltage) + ", Percentage: " + str(self.percentage))
 
 
 tmp = ANALOG_DIGITAL_CONVERTER()
