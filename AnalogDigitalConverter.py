5ca8dce20b60a1f3a66fe325a4c22b4e654507ed
Lukas Uhrig <lukas.uhrig2000@gmail.com>
2021-03-04 09:39:19 +0100

    adc

---
+++ AnalogDigitalConverter.py
@@ -0,0 +1,19 @@
+from ADS1x15 import ADS1015
+import threading
+import time
+
+class ANALOG_DIGITAL_CONVERTER(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.adc = ADS1015()
+        self.channel = 0
+        self.gain = 1
+        self.data = 0
+        self.start()
+
+
+    def run(self):
+        while True:
+            self.data = self.adc.read_adc(self.channel, self.gain)
+            time.sleep(0.1)
